c369f3350b7db964ca95419d4118cdfb66a3dff4
911_Astalus_Adrian <72087564+911AstalusAdrian@users.noreply.github.com>
2021-02-27 16:24:32 +0200

    Location

--- static/js/new_landmark.js
+++ static/js/new_landmark.js
@@ -1,5 +1,10 @@
 function add_landmark(){
+    document.getElementById("overlay").style.display="block";
     // get data from inputs
+    var landmark_name=document.getElementById("landmark_name").value;
+    var short_description=document.getElementById("short_description").value;
+    var landmark_tags=document.getElementById("landmark_tags").value;
+
     if(false){
         //validate data
     }
@@ -39,3 +44,35 @@ function add_landmark(){
     }
 } 
 }
+
+function geoFindMe() {
+
+    const status = document.querySelector('#landmark_location');
+    const mapLink = document.querySelector('#map-link');
+  
+    mapLink.href = '';
+    mapLink.textContent = '';
+  
+    function success(position) {
+      const latitude  = position.coords.latitude;
+      const longitude = position.coords.longitude;
+  
+      status.textContent = '';
+      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    }
+  
+    function error() {
+      status.textContent = 'Unable to retrieve your location';
+    }
+  
+    if(!navigator.geolocation) {
+      status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+      status.textContent = 'Locating…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
+  }
+  
+  document.querySelector('#find-me').addEventListener('click', geoFindMe);
